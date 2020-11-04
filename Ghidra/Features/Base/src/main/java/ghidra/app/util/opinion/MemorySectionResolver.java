f51e090398dc95a2fbaec5e6ec84d290889ced9f
Danil6969 <20danil02@mail.ru>
2020-11-04 16:06:04 +0300

    Update MemorySectionResolver.java

--- Ghidra/Features/Base/src/main/java/ghidra/app/util/opinion/MemorySectionResolver.java
+++ Ghidra/Features/Base/src/main/java/ghidra/app/util/opinion/MemorySectionResolver.java
@@ -607,8 +607,13 @@ public abstract class MemorySectionResolver {
 					conflictGapEnd = expectedRangeStart.previous();
 				}
 
-				Address rangeStartAddr =
-					fileRange.rangeStartAddress.add(filePos - fileRange.rangeStartFileOffset);
+				Address rangeStartAddr;
+				try {
+					rangeStartAddr = fileRange.rangeStartAddress.add(filePos - fileRange.rangeStartFileOffset);
+				}
+				catch (AddressOutOfBoundsException e) {
+					rangeStartAddr = fileRange.rangeStartAddress;
+				}
 
 				// Ignore use of overlay and compare physical address for match to avoid duplication
 				if (!expectedRangeStart.getPhysicalAddress().equals(
